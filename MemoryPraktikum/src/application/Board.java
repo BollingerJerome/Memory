03aa97fee961b794e24e7a0366541fc95a6a2e96
BollingerJerome <73817608+BollingerJerome@users.noreply.github.com>
2020-11-10 16:33:33 +0100

    random card distribution. With errrors

--- MemoryPraktikum/src/application/Board.java
+++ MemoryPraktikum/src/application/Board.java
@@ -1,73 +1,165 @@
 package application;
 
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Board {
 	
-	public Board(Color[][] setup, int verticalCards, int horizontalCards, double scalarX, double scalarY, GraphicsContext gc) {
-		this.gc = gc;
-		this.setup = setup;
-		this.verticalCards = verticalCards;
-		this.horizontalCards = horizontalCards;
-		this.scalarX = scalarX;
-		this.scalarY = scalarY;
-		this.card = new Card[horizontalCards][verticalCards];
-		
-		for (int i = 0; i<horizontalCards; i++) {
-			for (int j = 0; j<verticalCards; j++) {
-				this.card[i][j] = new Card(gc, i*scalarX, j*scalarY, scalarX, scalarY, setup[i][j]);
-			}
-		}
-	}
-	
-	private Color[][] setup;
-	private int verticalCards, horizontalCards;
-	private double scalarX, scalarY;
-	private Card[][] card;
-	private GraphicsContext gc;
-	
-	public void drawBoard() {
-		for (int i = 0; i<horizontalCards; i++) {
-			for (int j = 0; j<verticalCards; j++) {
-				this.card[i][j].draw();
-			}
-		}
-	}
-	
-	public Color[][] getSetup() {
-		return setup;
-	}
-	public void setSetup(Color[][] setup) {
-		this.setup = setup;
-	}
-	public int getVerticalCards() {
-		return verticalCards;
-	}
-	public void setVerticalCards(int verticalCards) {
-		this.verticalCards = verticalCards;
-	}
-	public int getHorizontalCards() {
-		return horizontalCards;
-	}
-	public void setHorizontalCards(int horizontalCards) {
-		this.horizontalCards = horizontalCards;
-	}
-	public double getScalarX() {
-		return scalarX;
-	}
-	public void setScalarX(double scalarX) {
-		this.scalarX = scalarX;
-	}
-	public double getScalarY() {
-		return scalarY;
-	}
-	public void setScalarY(double scalarY) {
-		this.scalarY = scalarY;
-	}
-	
-	
 	
 	
 
+	public Board(Color[][] colors, double horizontalTiles, double verticalTiles, double width, double heigth) {
+		super();
+		this.colors = colors;
+		this.horizontalTiles = horizontalTiles;
+		this.verticalTiles = verticalTiles;
+		this.width = width;
+		this.heigth = heigth;
+		this.rectangleField = new Rectangle[(int) horizontalTiles][(int) verticalTiles];
+		
+		double rectWidth = width/horizontalTiles;
+		double rectHeigth = heigth/verticalTiles;
+		for (int i = 0; i< horizontalTiles; i++) {
+			for (int j = 0; j<verticalTiles; j++) {
+				rectangleField[i][j] = new Rectangle(i*rectWidth, j*rectHeigth, rectWidth, rectHeigth);
+				rectangleField[i][j].setFill(colors[i][j]); //why isn't there a constructor for five variables?
+			}
+		}
+	}
+	
+	//random color constructor
+	public Board(double horizontalTiles, double verticalTiles, double width, double heigth) {
+		this.horizontalTiles = horizontalTiles;
+		this.verticalTiles = verticalTiles;
+		this.width = width;
+		this.heigth = heigth;
+		this.rectangleField = new Rectangle[(int) horizontalTiles][(int) verticalTiles];
+		this.colors = randomize();
+		double rectWidth = width/horizontalTiles;
+		double rectHeigth = heigth/verticalTiles;
+		for (int i = 0; i< horizontalTiles; i++) {
+			for (int j = 0; j<verticalTiles; j++) {
+				rectangleField[i][j] = new Rectangle(i*rectWidth, j*rectHeigth, rectWidth, rectHeigth);
+				rectangleField[i][j].setFill(colors[i][j]); //why isn't there a constructor for five variables?
+			}
+		}
+	}
+	
+	
+	private Color[][] colors;
+	private double horizontalTiles, verticalTiles, width, heigth;
+	private Rectangle rectangleField[][];	
+	
+	public Color[][] randomize () {
+		int tiles = (int) horizontalTiles * (int) verticalTiles;
+		
+		Color[][] randomColors = new Color[(int) horizontalTiles][(int) verticalTiles];
+		double[][] tile = new double[3][tiles];
+		
+		
+		//each tile gets two random floats
+		for (int i = 0; i < tiles; i++) {
+			tile[0][i] = i; 
+			tile[1][i] = Math.random();
+			tile[2][i] = Math.random();
+		}
+		
+		//Bubble sort
+		for (int i = 0; i < tiles; i++) {
+			for(int j = 0; j < tiles-1; j++) {
+				if(tile[1][j] > tile[1][j+1]) {
+					
+					double helperZero = tile[0][j+1];
+					double helperOne = tile[1][j+1];
+					double helperTwo = tile[2][j+1];
+					tile[0][j+1] = tile[0][j];
+					tile[1][j+1] = tile[1][j];
+					tile[2][j+1] = tile[2][j];
+					tile[0][j] = helperZero;
+					tile[1][j] = helperOne;
+					tile[2][j] = helperTwo;
+				}
+			}
+		}
+		
+		//nummerieren
+		for(int j = 0; j < tiles; j++) {
+			tile[1][j] = j%horizontalTiles;
+		}
+		
+		//sortieren zweiten Reihe
+		for (int i = 0; i < tiles; i++) {
+			for(int j = 0; j < tiles-1; j++) {
+				
+				if(tile[2][j] > tile[2][j+1]) {
+					
+					double helperZero = tile[0][j+1];
+					double helperOne = tile[1][j+1];
+					double helperTwo = tile[2][j+1];
+					tile[0][j+1] = tile[0][j];
+					tile[1][j+1] = tile[1][j];
+					tile[2][j+1] = tile[2][j];
+					tile[0][j] = helperZero;
+					tile[1][j] = helperOne;
+					tile[2][j] = helperTwo;
+				}
+			}
+		}
+		//nummerieren
+		for(int j = 0; j < tiles; j++) {
+			tile[2][j] = j/horizontalTiles;
+		}
+		
+		for (int i = 0; i < tiles; i++) {
+			int hor = (int) tile[1][i];
+			int ver = (int) tile[2][i];
+			int col = (int) tile[0][i];
+			Color which = TileColors.getColortiles()[col];
+			System.out.println(i+".");
+			System.out.println(hor +"|"+ver);
+			System.out.println();
+			randomColors[hor][ver] = which; 
+		}
+		
+		return randomColors;	
+	}
+	
+	
+	public Color[][] getColors() {
+		return colors;
+	}
+	public void setColors(Color[][] colors) {
+		this.colors = colors;
+	}
+	public double getHorizontalTiles() {
+		return horizontalTiles;
+	}
+	public void setHorizontalTiles(double horizontalTiles) {
+		this.horizontalTiles = horizontalTiles;
+	}
+	public double getVerticalTiles() {
+		return verticalTiles;
+	}
+	public void setVerticalTiles(double verticalTiles) {
+		this.verticalTiles = verticalTiles;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	public double getHeigth() {
+		return heigth;
+	}
+	public void setHeigth(double heigth) {
+		this.heigth = heigth;
+	}
+	public Rectangle[][] getRectangleField() {
+		return rectangleField;
+	}
+	public void setRectangleField(Rectangle[][] rectangleField) {
+		this.rectangleField = rectangleField;
+	}
+	
 }
--- MemoryPraktikum/src/application/Main.java
+++ MemoryPraktikum/src/application/Main.java
@@ -4,12 +4,14 @@ package application;
 //hat es geklappt?
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 
 public class Main extends Application {
@@ -20,17 +22,23 @@ public class Main extends Application {
 			
 			
 			BorderPane root = new BorderPane();
-			Canvas canvas = new Canvas(500, 500);
+			Canvas canvas = new Canvas(800, 800);
 			GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-			
+			Group bord = new Group();
 			Color[][] field = {{Color.RED, Color.BLUE, Color.RED},
 					{Color.BLUE,Color.RED, Color.BLUE},
 					{Color.RED, Color.BLUE, Color.RED}
 					};
 			
-			Board board = new Board(field, 3, 3, canvas.getWidth()/3, canvas.getHeight()/3, graphicsContext);
-			board.drawBoard();
-			root.setCenter(canvas);
+			Board board = new Board( 4, 5, canvas.getWidth()/4, canvas.getHeight()/4);
+			
+			for (int i = 0; i< board.getHorizontalTiles(); i++) {
+				for (int j = 0; j<board.getVerticalTiles(); j++) {
+					bord.getChildren().add(board.getRectangleField()[i][j]);
+				}
+			}
+			
+			root.setCenter(bord);
 			primaryStage.setScene(new Scene(root));
 			primaryStage.setTitle("Welcome to Memory!");
 			primaryStage.show();
